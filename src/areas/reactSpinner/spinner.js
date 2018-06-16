44f93d9d5820f54bc4aa15387a78dac81e3ce202
haydenty <htwade51@gmail.com>
2018-06-15 22:12:42 -0500

    init react code with router

---
+++ src/areas/reactSpinner/spinner.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Spinner extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+
+      </div>
+    );
+  }
+}
+
+export default Spinner;
