92140089b040e292b20b76715980e35aa85414a6
haydenty <htwade51@gmail.com>
2018-06-17 20:57:11 -0500

    resume content and change all font the same

---
+++ src/areas/resume/content.js
@@ -0,0 +1,33 @@
+const content = [
+    {
+        "title":"SOFTWARE ENGINEER II - NAVISPHERE VISION",
+        "subtitle":"C.H. Robinson, Eden Prairie, Minnesota, January 2018 - Current",
+        "body":"Enhanced eCommerce application that provides customers a view into their global supply chain. Worked closely with data scientists to provide in-app predictive analytics. Designed, implemented, and tested web application to help onboard new customers using React.js and Redux. Built software to consume data from enterprise service bus (ESB). Mentored a growing team of developers. Implemented new customers on a strict deadline. Primary code utilization included HTML5, CSS3, AngularJS, Node.js, MongoDB, Elasticsearch, Enzyme, Jest, Nightwatch.js, and C#."
+    },
+    {
+        "title":"SOFTWARE ENGINEER I - GLOBAL FORWARDING",
+        "subtitle":"C.H. Robinson, Eden Prairie, Minnesota, May 2016 - December 2017",
+        "body":"Worked on a team of 5 to design and implement micro-services to send and receive messages from U.S. Customs (ABI CATAIR). Refactored Pre-Arrival Processing System (PAPS) Angular web application with a responsive, mobile first design to enhance UX across wide range of devices used by truck drivers. Contributed to team of ~20 developers by leading Angular training sessions, being DevOps Advocate, and defining code standards for web applications. Built nuget package to support Role Based Access Control (RBAC) using Auth0 and ServiceStack request attributes and plugins. Built shared Angular component library using npm. Leveraged unit tests and automated acceptance(e2e) tests to ensure code quality via CI/CD pipeline. Mentored interns as part of the Intern Program Committee. Primary code utilization included HTML5, Bootstrap, Angular, Protractor.js, Jasmine, C#, ServiceStack, and SQL."
+    },
+    {
+        "title":"PROGRAMMER ANALYST INTERN - GLOBAL FORWARDING",
+        "subtitle":"C.H. Robinson, Eden Prairie, Minnesota, May 2015 - May 2016",
+        "body":"Gathered requirements from users, designed, implemented, and tested web application to support team's software deployment process. Used AngularJS, SQL, ServiceStack, C# to develop web application. Worked remotely for half of the internship where I contributed to sprint work along side scrum team. Presented products to executives, managers, and various teams."
+    },
+    {
+        "title":"SOFTWARE ENGINEER",
+        "subtitle":"TenEx, LLC, Platteville, Wisconsin , February 2015 - August 2015",
+        "body":"Designed and implemented analytics solution to help understand user behavior and rate of adoption. Created daily and weekly reports using Java and MySql with Slack integrations."
+    },
+    {
+        "title":"SOFTWARE ENGINEER INTERN",
+        "subtitle":"Danfoss, Loves Park, IL, May 2014 - August 2014",
+        "body":"Designed and implemented embedded Wi-Fi communication system with mobile interface. Prototyped using ST Discovery Kit microcontroller and Wi-Fi module. Primary code utilization included HTML5, CSS3, JavaScript, and C."
+    }
+    // {
+    //     "title":"",
+    //     "subtitle":"",
+    //     "body":""
+    // }
+];
+export default content;
