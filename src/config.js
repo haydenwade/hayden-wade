2ab7683bc3187cd1a8847896d0e5976a8053459f
haydenty <htwade51@gmail.com>
2018-08-03 01:08:57 -0500

    updated config for deploy

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 const config = {
-    viApi:'http://localhost:3001',
+    viApi: process.env.VI_API_URL || 'http://localhost:3001',
     auth:{
         siteDomain:'https://haydenwade.com',
         domain: 'haydenty.auth0.com',
         clientID: 'UninCDiPheTiAcFFo0Yt2Nb7nORk8ZTf',
-        redirectUri: 'http://localhost:3000/callback',
+        redirectUri: process.env.AUTH_REDIRECT_URL || 'http://localhost:3000/callback',
         audience: 'http://vi.com',
     }
 };
